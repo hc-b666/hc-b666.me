f844cd1cfd6064f420b7124ecfaf560a1469afc2
Bobur <bobur0218programmer@gmail.com>
2024-01-16 00:28:00 +0500

    js for forms

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 const bannerContent = document.getElementById("bannerContent");
+const contactMeForm = document.getElementById("contactMeForm");
+const btnContactMe = document.getElementById("btnContactMe");
+const btnContactClose = document.getElementById("btnContactClose");
 
 // setInterval(function() {}, delay in milliseconds)
 // (first argument - function), (second argument - delay)  
@@ -10,3 +13,17 @@ let restartAnimation = function ()
 }
 
 setInterval(restartAnimation, 10000);
+
+// Functions for Opening and Closing Contact Forms
+function showContactForms () 
+{
+    contactMeForm.style.display = "inline-block";
+}
+
+function closeContactForms () 
+{
+    contactMeForm.style.display = "none";
+}
+
+btnContactMe.onclick = showContactForms;
+btnContactClose.onclick = closeContactForms;
