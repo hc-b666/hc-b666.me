d8dbece144ed1cda8432ea120334e8442e86e996
Bobby <bobur0218programmer@gmail.com>
2024-07-27 10:51:30 +0500

    update cv

--- src/lib/handleDownload.ts
+++ src/lib/handleDownload.ts
@@ -1,7 +1,7 @@
 const handleDownload = () => {
   const link = document.createElement("a");
-  link.href = "/files/CV-Muhammadbobur-Abdukarimov.pdf";
-  link.download = "CV-Muhammadbobur-Abdukarimov.pdf";
+  link.href = "/files/CV_Muhammadbobur_Abdukarimov.pdf";
+  link.download = "CV_Muhammadbobur_Abdukarimov.pdf";
   link.click();
 };
 
